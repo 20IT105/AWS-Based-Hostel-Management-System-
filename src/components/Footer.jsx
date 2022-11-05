968143166c9cf4a19f9ad21afd110f87035b88df
Patel Pratham <20IT120@charusat.edu.in>
2022-11-05 07:55:22 +0530

    Hostel_Management

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="text-center py-2">
+           <h6><b>Devloper's Contact</b></h6> 
+           <p><b>Mobile:123456789  ||  Email:htdjfx@gmail.com</b></p>
+        </footer>
+    );
+};
+
+export default Footer;
